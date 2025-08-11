cd7c4e13e054a6590e1775bdb1c6cbe014425ce0
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-08-11 11:31:14 -0700

    Initial Commit

---
+++ EnhancedRadarDemo/EnhancedRadarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedRadarDemoApp.swift
+//  EnhancedRadarDemo
+//
+//  Created by Yuhao Chen on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedRadarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
